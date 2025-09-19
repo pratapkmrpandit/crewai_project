e09b9b5621498a6d9d7722898e4714a6f21d0514
Pratap kumar <pratapkmrpandit@gmail.com>
2025-09-19 14:30:19 +0530

    update helper to load serper key

--- helper.py
+++ helper.py
@@ -4,4 +4,7 @@ from dotenv import load_dotenv
 def load_env():
     """Load environment variables from .env"""
     load_dotenv()
-    return os.getenv("GEMINI_API_KEY")
+    return {
+        "gemini_api_key": os.getenv("GEMINI_API_KEY"),
+        "serper_api_key": os.getenv("SERPER_API_KEY")
+    }
